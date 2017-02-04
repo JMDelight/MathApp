da6ea1caaca470d941975be66c2a8222a61ff8ba
Joel Delight <thefencingflutist@gmail.com>
2017-02-04 09:59:53 -0800

    set up a basic multiplacation app.

---
+++ Assets/Scripts/CheckAnswer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckAnswer : MonoBehaviour {
+
+  private Text answer;
+  private Text question;
+  private int correctAnswer;
+  public int score = 0;
+  public int numberAnswered = 0;
+
+  // Use this for initialization
+  void Start () {
+    question = GameObject.Find("Question").GetComponent<Text>();
+    answer = GameObject.Find("Answer").GetComponent<Text>();
+    CreateQuestion();
+
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  public void Validate() {
+    if(answer.text == correctAnswer.ToString()) {
+      Debug.Log("Correct");
+      score++;
+    } else {
+      Debug.Log("Incorrect");
+    }
+    numberAnswered++;
+    answer.text = "";
+    CreateQuestion();
+  }
+
+  public void CreateQuestion() {
+    int numberOne = Random.Range(0, 11);
+    int numberTwo = Random.Range(0, 11);
+    correctAnswer = numberOne * numberTwo;
+    question.text = numberOne.ToString() + " X " + numberTwo.ToString();
+  }
+}
---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Input : MonoBehaviour {
+
+  private int value;
+  public Text textObject;
+  private Text answer;
+
+  // Use this for initialization
+  void Start() {
+    answer = GameObject.Find("Answer").GetComponent<Text>();
+    //value = Int32.Parse(GetComponent<Text>().text);
+    Debug.Log(textObject.text);
+    Debug.Log(answer.text);
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void isPushed() {
+    if(textObject.text == "Clear") {
+      answer.text = "";
+    } else if(textObject.text == "Delete") {
+      string currentAnswer = answer.text;
+      
+      answer.text = currentAnswer.Substring(0, currentAnswer.Length - 1);
+    } else {
+      string currentAnswer = answer.text;
+      currentAnswer += textObject.text;
+      answer.text = currentAnswer;
+    }
+    
+  }
+}
